50ac0cf44812f150693bed8631776c8141d75386
Harsharan Rakhra <overlordharsharan@gmail.com>
2025-11-13 10:58:32 -0500

    added tf publisher

--- src/slam_launch/launch/slam.launch.py
+++ src/slam_launch/launch/slam.launch.py
@@ -1,15 +1,30 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import TimerAction
+import os
 
 def generate_launch_description():
+
+    urdf_path = os.path.join(
+        os.getenv('COLCON_PREFIX_PATH').split(':')[0],
+        'robot_description/share/robot_description/robot.urdf.xacro'
+    )
+
     return LaunchDescription([
-        # Node that starts immediately
+        # Start IMU node early to allow it to calibrate
         Node(
             package='sensors',
             executable='imu_publisher',
             name='imu_publisher',
             output='screen'
+        ),
+
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': open(urdf_path).read()}]
         ),
 
         # Node that starts after 3 seconds to allow imu to calibrate
