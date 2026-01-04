1fad9f374e5994e836b4464aa54bcd6b357488da
Harsharan Rakhra <overlordharsharan@gmail.com>
2026-01-04 10:53:55 -0500

    prints the devices found to confirm and added to env var

--- scripts/setup_i2c.sh
+++ scripts/setup_i2c.sh
@@ -10,4 +10,7 @@ sudo apt install -y i2c-tools python3-smbus python3-pip
 echo "Enabling I2C interface..."
 sudo raspi-config nonint do_i2c 0
 
+echo "Devices found:"
+sudo i2cdetect -y 1 && export MPU_BUS_ADR=0x68
+
 echo "All done! Please reboot to finish setup."
