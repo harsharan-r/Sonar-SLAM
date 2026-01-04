17bca17e6dd7d03c8ce0716c2785093d576b971f
Harsharan Rakhra <overlordharsharan@gmail.com>
2026-01-04 11:06:15 -0500

    fixed launch to only delay nodes that affect imu calibration

--- src/slam_launch/launch/slam.launch.py
+++ src/slam_launch/launch/slam.launch.py
@@ -28,28 +28,30 @@ def generate_launch_description():
         ),
         
         Node(
-                    package='foxglove_bridge',
-                    executable='foxglove_bridge',
-                    name='foxglove_bridge',
-                    output='screen'
+            package='foxglove_bridge',
+            executable='foxglove_bridge',
+            name='foxglove_bridge',
+            output='screen'
+        ),
+
+        Node(
+            package='slam',
+            executable='slam_publisher',
+            name='slam_publisher',
+            output='screen'
         ),
 
         # Node that starts after 3 seconds to allow imu to calibrate
         TimerAction(
-            period=3.0,  # delay in seconds
+            period=6.0,  # delay in seconds
             actions=[
-                # Node(
-                #     package='sensors',
-                #     executable='sonar_publisher',
-                #     name='sonar_publisher',
-                #     output='screen'
-                # ),
                 Node(
-                    package='slam',
-                    executable='slam_publisher',
-                    name='slam_publisher',
+                    package='sensors',
+                    executable='sonar_publisher',
+                    name='sonar_publisher',
                     output='screen'
-                )
+                ),
             ]
         ),
+
     ])
